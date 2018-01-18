b1a6388b9991daece6b202a17f281ddcaae478c7
Tomasz Kielar <tkielar@pgs-soft.com>
2018-01-18 13:22:13 +0100

    Web service

---
+++ PGS.Azure.ServiceFabric.VotingWeb/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PGS.Azure.ServiceFabric.VotingWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
---
+++ PGS.Azure.ServiceFabric.VotingWeb/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module('VotingApp', []);
+app.run(function () { });
+
+app.controller('VotingAppController', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
+
+    $scope.refresh = function () {
+        $http.get('api/Votes')
+            .then(function (data) {
+                $scope.votes = data;
+            });
+    };
+
+    $scope.remove = function (item) {
+        $http.delete('api/Votes/' + item)
+            .then(function() {
+                $scope.refresh();
+            });
+    };
+
+    $scope.add = function (item) {
+        $http.put('api/Votes/' + item)
+            .then(function () {
+                $scope.refresh();
+                $scope.item = undefined;
+            });
+    };
+}]);
